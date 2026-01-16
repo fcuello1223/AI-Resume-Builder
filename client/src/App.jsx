2040f47758a6d341f3075174dab729485a201ea2
Francisco Cuello <fcuel001@fiu.edu>
2026-01-16 15:03:11 -0500

    Set up basic routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import ResumeBuilder from "./pages/ResumeBuilder";
+import Preview from "./pages/Preview";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div></div>
-  )
-}
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="app" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="builder/:resumeId" element={<ResumeBuilder />} />
+        </Route>
+        <Route path="view/:resumeId" element={<Preview />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </React.Fragment>
+  );
+};
 
-export default App
+export default App;
