81200d38b1112ca1c7cb76f3a201e200b151ced2
Francisco Cuello <fcuel001@fiu.edu>
2026-01-18 21:06:22 -0500

    Completed Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import Banner from "../components/home/Banner";
+import Hero from "../components/home/Hero";
+import Features from "../components/home/Features";
+import Testimonial from "../components/home/Testimonial";
+import CallToAction from "../components/home/CallToAction";
+import Footer from "../components/home/Footer";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Banner />
+      <Hero />
+      <Features />
+      <Testimonial />
+      <CallToAction />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
