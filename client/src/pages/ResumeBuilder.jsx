a846eea48b6193f09178d40d7bed35535488191d
Francisco Cuello <fcuel001@fiu.edu>
2026-02-11 15:09:05 -0500

    Added Functionality to make resumé public/private, to download it, and to make it shareable

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -10,6 +10,10 @@ import {
   Sparkles,
   ChevronLeft,
   ChevronRight,
+  Eye,
+  EyeOff,
+  Download,
+  Share2
 } from "lucide-react";
 
 import { dummyResumeData } from "../assets/assets";
@@ -91,6 +95,25 @@ const ResumeBuilder = () => {
   useEffect(() => {
     loadResumeData();
   }, []);
+
+  const changeResumeVisibility = async () => {
+    setResumeData({ ...resumeData, public: !resumeData.public });
+  };
+
+  const handleShare = () => {
+    const frontendUrl = window.location.href.split("/app/")[0];
+    const resumeUrl = frontendUrl + "/view/" + resumeId;
+
+    if (navigator.share) {
+      navigator.share({ url: resumeUrl, text: "My Resumé" });
+    } else {
+      alert("Share not supported on this browser!");
+    }
+  };
+
+  const downloadResume = () => {
+    window.print();
+  };
 
   return (
     <div>
@@ -232,11 +255,33 @@ const ResumeBuilder = () => {
                   />
                 )}
               </div>
+              <button className="bg-linear-to-br from-green-100 to-green-200 ring-green-300 text-green-600 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm">
+                Save Changes
+              </button>
             </div>
           </div>
           {/* Right Side (Preview) */}
           <div className="lg:col-span-7 max-lg:mt-6">
-            <div>{/* Buttons */}</div>
+            <div className="relative w-full">
+              <div className="absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2">
+                {resumeData.public && (
+                  <button onClick={handleShare} className="flex items-center p-2 px-4 gap-2 text-xs bg-linear-to-br from-blue-100 to-blue-200 text-blue-600 ring-blue-300 rounded-lg hover:ring transition-colors">
+                    <Share2 className="size-4" /> Share
+                  </button>
+                )}
+                <button onClick={changeResumeVisibility} className="flex items-center p-2 px-4 gap-2 text-xs bg-linear-to-br from-purple-100 to-purple-200 text-purple-600 ring-purple-300 rounded-lg hover:ring transition-colors">
+                  {resumeData.public ? (
+                    <Eye className="size-4" />
+                  ) : (
+                    <EyeOff className="size-4" />
+                  )}
+                  {resumeData.public ? "Public" : "Private"}
+                </button>
+                <button onClick={downloadResume} className="flex items-center gap-2 px-6 py-2 text-xs bg-linear-to-br from-green-100 to-green-200 text-green-600 ring-green-300 rounded-lg hover:ring transition-colors">
+                  <Download className="size-4" /> Download
+                </button>
+              </div>
+            </div>
             <ResumePreview
               data={resumeData}
               template={resumeData.template}
